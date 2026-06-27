b64bfed174fd0ca13d6298fb419ce51004a64b27
TienNhat5103 <nhatkontum123@gmail.com>
2026-06-28 02:17:34 +0700

    update search engine

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from .food_router import router as food_router  
 from .merchant_router import router as merchant_router
+from .search_engines import router as search_router
 
 api_router = APIRouter()
 
 # Đăng ký router đồ ăn vào hệ thống API chính
 api_router.include_router(merchant_router)
 api_router.include_router(food_router)
+api_router.include_router(search_router)
